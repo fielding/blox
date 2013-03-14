637fe756e485576cf8b28a75d4b21d09b6527772
Fielding Johnston <fielding@justfielding.com>
2013-03-14 13:31:56 -0500

    Initial Commit

---
+++ Tetris/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tetris
+//
+//  Created by Fielding Johnston on 3/14/13.
+//  Copyright (c) 2013 Fielding Johnston. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Tetris/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Tetris
+//
+//  Created by Fielding Johnston on 3/14/13.
+//  Copyright (c) 2013 Fielding Johnston. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+  return NSApplicationMain(argc, (const char **)argv);
+}
